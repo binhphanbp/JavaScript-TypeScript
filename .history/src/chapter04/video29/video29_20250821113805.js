0ea3e1e02b869b82967e46913adb1c4d3c8291e7
binhphan <phanbinhfedev150504@gmail.com>
2025-08-21 11:38:56 +0700

    Keyword BREAK

---
+++ .history/src/chapter04/video29/video29_20250821113805.js
@@ -0,0 +1,6 @@
+console.log('Video 29');
+
+// break
+for (let i = 0; i < 10; i++) {
+  console.log('i =', i);
+}
---
+++ .history/src/chapter04/video29/video29_20250821113817.js
@@ -0,0 +1,9 @@
+console.log('Video 29');
+
+// break
+for (let i = 0; i < 10; i++) {
+  console.log('i =', i);
+  if (i === 5) {
+    break;
+  }
+}
