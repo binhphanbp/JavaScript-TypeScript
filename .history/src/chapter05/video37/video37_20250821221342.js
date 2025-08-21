fa10a2bd82b3a76449f116fef0444403ee91918e
binhphan <phanbinhfedev150504@gmail.com>
2025-08-21 22:15:07 +0700

    Array

---
+++ .history/src/chapter05/video37/video37_20250821221342.js
@@ -0,0 +1,4 @@
+console.log('Video 37');
+
+const name = ['Phan Đức Bình', 'bianbp', 'Ngô Thị Kim Anh'];
+console.log(name);
---
+++ .history/src/chapter05/video37/video37_20250821221428.js
@@ -0,0 +1,6 @@
+console.log('Video 37');
+
+const name = ['Phan Đức Bình', 'bianbp', 'Ngô Thị Kim Anh'];
+console.log(name);
+
+const others = ['Bình Phan', true, 21, [15, 5, 2004]];
